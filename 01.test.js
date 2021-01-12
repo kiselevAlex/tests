b62adc68ec8f792706cb4a4a199c43587d58a94a
kiselev <akiselev@fix.ru>
2021-01-12 21:51:54 +0300

    fix test

--- 01.test.js
+++ 01.test.js
@@ -7,10 +7,18 @@ describe('signin page', () => {
 
     it('first render', async () => {
         await page.waitForSelector('[name="q"]');
-        await page.focus('[name="q"]');
+        const input = await page.$('[name="q"]')
+        await input.focus();
         await page.keyboard.type('search');
+
+        const value = await page.evaluate(el => el.value, input);
+        expect(value).toBe('search');
+
         await page.waitForXPath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[3]/div[1]/span[1]');
-        await page.click('[name="q"]', { clickCount: 3 });
-        await page.keyboard.type('');
+        const [clear] = await page.$x('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[3]/div[1]/span[1]');
+        await clear.click();
+
+        const value2 = await page.evaluate(el => el.value, input);
+        expect(value2).toBe('');
     });
 });
